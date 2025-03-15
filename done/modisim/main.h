6439411977a293bb562974248624cfc8c1e4ad38
Gillian <113825139+SaltyJill@users.noreply.github.com>
2025-03-15 13:08:21 +0800

    Add files via upload

---
+++ done/modisim/main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+#include "stc15f2k60s2.h"
+#include "absacc.h"
+#include "intrins.h"
+
+#define u8 unsigned char 
+#define s8 signed char
+#define u16 unsigned int
+#define s16 signed int
+#define u32 unsigned long
+#define s32 signed long
+
+#endif
